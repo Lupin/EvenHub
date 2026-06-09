560e389266536e01f964fd731b825a774862f4af
Gael Abegg Gauthey <Abegg.gauthey@gmail.com>
2026-06-09 22:59:48 +0200

    fix: DOUBLE_CLICK only

--- fasting-tracker/src/input.ts
+++ fasting-tracker/src/input.ts
@@ -1,7 +1,6 @@
 import type { EvenAppBridge } from '@evenrealities/even_hub_sdk'
 import { OsEventTypeList } from '@evenrealities/even_hub_sdk'
-import { rebuildCurrentMode } from './display'
-import { loadConfig, saveConfig } from './storage'
+import { toggleDisplayMode, rebuildCurrentMode } from './display'
 
 export function setupInputHandlers(bridge: EvenAppBridge): () => void {
   const unsubscribe = bridge.onEvenHubEvent((event) => {
@@ -13,23 +12,9 @@ export function setupInputHandlers(bridge: EvenAppBridge): () => void {
         ? evt.eventType
         : OsEventTypeList.fromJson(evt.eventType)
 
-    const config = loadConfig()
-
-    switch (type) {
-      case OsEventTypeList.SCROLL_TOP_EVENT:
-        if (config.displayMode !== 'text') {
-          config.displayMode = 'text'
-          saveConfig(config)
-          rebuildCurrentMode(bridge)
-        }
-        break
-      case OsEventTypeList.SCROLL_BOTTOM_EVENT:
-        if (config.displayMode !== 'timeline') {
-          config.displayMode = 'timeline'
-          saveConfig(config)
-          rebuildCurrentMode(bridge)
-        }
-        break
+    if (type === OsEventTypeList.DOUBLE_CLICK_EVENT) {
+      toggleDisplayMode()
+      rebuildCurrentMode(bridge)
     }
   })
 
